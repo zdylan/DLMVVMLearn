6c5e0a745b37c04c994bb0038d1bfce9beed2d20
dylan <dylan.zhang111@gmail.com>
2017-06-27 23:28:10 +0800

    add initial version.

---
+++ DLMVVMLearn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DLMVVMLearn
+//
+//  Created by dylan on 2017/6/27.
+//  Copyright © 2017年 dylan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
